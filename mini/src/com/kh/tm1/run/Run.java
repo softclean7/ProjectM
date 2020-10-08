950073297207bb188943d5c19cd3de82698b8e1b
yunhae_Lee <yunhae_lee@naver.com>
2020-10-08 13:33:29 +0900

    2020 10 08 Test출력

--- mini/src/com/kh/tm1/run/Run.java
+++ mini/src/com/kh/tm1/run/Run.java
@@ -3,9 +3,9 @@ package com.kh.tm1.run;
 public class Run {
 
 	public static void main(String[] args) {
-	
-		// TODO Auto-generated method stub
 
+		// TODO Auto-generated method stub
+		System.out.println("test");
 	}
 
 }
